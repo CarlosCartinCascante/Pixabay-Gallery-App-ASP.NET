67076556a45ede538c7d10941d5e9bd6935e639d
Carlos Cartin <carlos.cartincascante@gmail.com>
2022-12-31 00:03:37 -0600

    Create a simple gallery app using API REST in ASP.NET

---
+++ Pixabay Gallery App/Pixabay Gallery App/Controllers/GalleryController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Newtonsoft.Json.Linq;
+
+using Pixabay_Gallery_App.Models;
+using Newtonsoft.Json;
+
+namespace Pixabay_Gallery_App.Controllers
+{
+    public class GalleryController : Controller
+    {
+
+        //Now define your asynchronous method which will retrieve all your pokemon.
+        public async Task<ActionResult> Index(string query)
+        {
+            if (query == null)
+                query = "";
+            //Define your baseUrl
+            string apiUrl = "https://pixabay.com/api/?key=32034970-fbd6a67c873fd7587e3f7c733&q=" + query;
+            //Have your using statements within a try/catch block
+            try
+            {
+                //We will now define your HttpClient with your first using statement which will implements a IDisposable interface.
+                using (HttpClient client = new())
+                {
+                    //In the next using statement you will initiate the Get Request, use the await keyword so it will execute the using statement in order.
+                    using (HttpResponseMessage res = await client.GetAsync(apiUrl))
+                    {
+                        //Then get the content from the response in the next using statement, then within it you will get the data, and convert it to a c# object.
+                        using (HttpContent content = res.Content)
+                        {
+                            //Now assign your content to your data variable, by converting into a string using the await keyword.
+                            var data = await content.ReadAsStringAsync();
+                            //If the data isn't null return log convert the data using newtonsoft JObject Parse class method on the data.
+                            if (content != null)
+                            {
+                                List<Image>? images = new();
+                                images = JsonConvert.DeserializeObject<List<Image>>(JObject.Parse(data)["hits"].ToString());
+                                return View(images);
+                            }
+                            else
+                            {
+                                Console.WriteLine("NO Data----------");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("NO Data-----");
+                Console.WriteLine(exception);
+            }
+            return View();
+        }
+
+    }
+}
---
+++ Pixabay Gallery App/Pixabay Gallery App/Models/Image.cs
@@ -0,0 +1,12 @@
+﻿namespace Pixabay_Gallery_App.Models
+{
+    public class Image
+    {
+        public int? Id { get; set; }
+        public string? Type { get; set; }
+
+        public string? WebFormatURL { get; set; }
+
+        public int? Likes { get; set; }
+    }
+}
